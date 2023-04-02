116682071e60209b97ee29fab2baff5e3a5de1db
Gullit Moran <gullitmoran@hotmail.com>
2023-04-01 23:23:30 -0500

    One Comint

--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -37,24 +37,24 @@ const countriesReducer = (state = initialState, action) => {
         ...state,
         filteredCountries: action.payload,
       };
-      case ORDER_COUNTRIES:
-        let sortedCountries = [];
-        if (state.continentFilter) {
-          sortedCountries =
-            action.payload === "Abc"
-              ? [...state.filteredCountries].sort((a, b) => a.name.localeCompare(b.name))
-              : [...state.filteredCountries].sort((a, b) => b.name.localeCompare(a.name));
-        } else {
-          sortedCountries =
-            action.payload === "Abc"
-              ? [...state.countries].sort((a, b) => a.name.localeCompare(b.name))
-              : [...state.countries].sort((a, b) => b.name.localeCompare(a.name));
-        }
-        return {
-          ...state,
-          filteredCountries: sortedCountries,
-          order: action.payload,
-        };
+    case ORDER_COUNTRIES:
+      let sortedCountries = [];
+      if (state.continentFilter) {
+        sortedCountries =
+          action.payload === "Abc"
+            ? [...state.filteredCountries].sort((a, b) => a.name.localeCompare(b.name))
+            : [...state.filteredCountries].sort((a, b) => b.name.localeCompare(a.name));
+      } else {
+        sortedCountries =
+          action.payload === "Abc"
+            ? [...state.countries].sort((a, b) => a.name.localeCompare(b.name))
+            : [...state.countries].sort((a, b) => b.name.localeCompare(a.name));
+      }
+      return {
+        ...state,
+        filteredCountries: sortedCountries,
+        order: action.payload,
+      };
     case FILTER_BY_CONTINENT:
       const filteredByContinent = action.payload
         ? state.countries.filter((c) => c.continent === action.payload)
@@ -69,16 +69,16 @@ const countriesReducer = (state = initialState, action) => {
         ...state,
         activities: [...state.activities, action.payload],
       };
-      case GET_ALL_ACTIVITIES:
-  return {
-    ...state,
-    activities: action.payload,
-  };
-  case SET_SELECTED_ACTIVITY:
-  return {
-    ...state,
-    selectedActivity: action.payload,
-  };
+    case GET_ALL_ACTIVITIES:
+      return {
+        ...state,
+        activities: action.payload,
+      };
+    case SET_SELECTED_ACTIVITY:
+      return {
+        ...state,
+        selectedActivity: action.payload,
+      };
 
     default:
       return state;
