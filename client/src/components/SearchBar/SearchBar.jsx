725013bebaa000f60391aa7ca3f2fcd5560262fb
Gullit Moran <gullitmoran@gmail.com>
2023-04-04 12:19:05 -0500

    first commit

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -7,15 +7,40 @@ const SearchBar = ({ show }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const dispatch = useDispatch();
 
-  const handleSearch = ({target}) => {
+  const handleSearch = ({ target }) => {
     const input = target.value;
-    const validInput = /^[a-zA-Z\s]*$/.test(input);
-    setSearchTerm(input);
-    if (!input || !validInput) {
+    // Validaciones
+    const validLength = input.length <= 50; // Longitud máxima de 50 caracteres
+    const validCharacters = /^[a-zA-Z\s]*$/.test(input); // Solo letras y espacios
+    const validSpecialCharacters = !/[@$!%*#?&]/.test(input); // No caracteres especiales
+    const validNumbers = !/\d/.test(input); // No números
+    const validSpaces = !/^\s/.test(input); // No espacios en blanco al inicio
+  
+    if (!validLength) {
+      alert('No se permite más de 50 caracteres');
       return;
     }
+    if (!validNumbers) {
+      alert('No se permiten números');
+      return;
+    }
+    if (!validCharacters) {
+      alert('No se permiten caracteres especiales');
+      return;
+    }
+    if (!validSpecialCharacters) {
+      alert('No se permiten caracteres especiales');
+      return;
+    }
+    if (!validSpaces) {
+      alert('No se permiten espacios en blanco al inicio');
+      return;
+    }
+  
+    setSearchTerm(input);
     dispatch(searchCountry(input));
   };
+  
 
   if (!show) {
     return null;
